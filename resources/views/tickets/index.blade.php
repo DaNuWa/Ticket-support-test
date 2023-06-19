145cb157c9d10325e3f30f2d45b6b893686eecc0
Yohan <yohan.dhanushka@colombo.rezgateway.com>
2023-06-19 18:09:32 +0530

    Ticket create ui implemented

---
+++ resources/views/tickets/index.blade.php
@@ -0,0 +1,115 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="container">
+                    <div class="card-header container m-2">{{ __('Create a new Ticket') }}</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <div>
+                            <div class="col-md-8">
+                                <div class="card">
+
+                                    <div class="card-body">
+                                        <form method="POST" action="{{ route('tickets.store') }}">
+                                            @csrf
+
+
+                                            <div class="row mb-3">
+                                                <label for="name"
+                                                       class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                                                <div class="col-md-6">
+                                                    <input id="name" type="text"
+                                                           class="form-control @error('name') is-invalid @enderror"
+                                                           name="password" value="{{ old('name') }}" required autocomplete="current-password">
+
+                                                    @error('name')
+                                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+                                            <div class="row mb-3">
+                                                <label for="email"
+                                                       class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                                                <div class="col-md-6">
+                                                    <input id="email" type="email"
+                                                           class="form-control @error('email') is-invalid @enderror"
+                                                           name="email" value="{{ old('email') }}" required
+                                                           autocomplete="email" autofocus>
+
+                                                    @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+                                            <div class="row mb-3">
+                                                <label for="contact"
+                                                       class="col-md-4 col-form-label text-md-end">{{ __('Contact number') }}</label>
+
+                                                <div class="col-md-6">
+                                                    <input id="contact" type="text"
+                                                           class="form-control @error('contact') is-invalid @enderror"
+                                                           name="contact" required >
+
+                                                    @error('contact')
+                                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+                                            <div class="row mb-3">
+                                                <label for="description"
+                                                       class="col-md-4 col-form-label text-md-end">{{ __('Ticket Description') }}</label>
+
+                                                <div class="col-md-6">
+                                                    <textarea
+                                                        id="description"
+                                                        class="form-control @error('password') is-invalid @enderror"
+                                                        name="description" required >
+                                                    </textarea>
+                                                    @error('description')
+                                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+
+                                            <div class="row mb-0">
+                                                <div class="col-md-8 offset-md-4">
+                                                    <button type="submit" class="btn btn-primary">
+                                                        {{ __('Submit') }}
+                                                    </button>
+
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('tickets', TicketController::class);
+
 
 Auth::routes();
 
